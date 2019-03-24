c6ce3349e095b36b8943b5657a09976fee655fad
MicaelaSuarezCortez <micaelasuarezco@gmail.com>
2019-03-23 21:59:37 -0500

    implementando test para links rotos y función respectiva

--- src/controller/stats.js
+++ src/controller/stats.js
@@ -2,8 +2,24 @@ export const statsLinks = (arrayObjetosLinks) => {
   let newArrayHref = [];  
   arrayObjetosLinks.forEach(element => newArrayHref.push(element.href));
   const totalLinks = newArrayHref.reduce((acum, item) => acum + (item.href !== ''), 0);
-  const uniqLinks = newArrayHref.filter((value, index, result) => result.indexOf(value) === index).length;  
+  const uniqLinks = newArrayHref.filter((value, index, array) => array.indexOf(value) === index).length;  
   return {totalLinks, uniqLinks};
 };
 
-// console.log(totalLinksStats(arrayObjLinks));
+// console.log(statsLinks(arrayObjLinks));
+
+
+export const linksBroken = (arrayObjetosValidate) => {
+  let newArrayStatusText = [];  
+  let broken = 0;
+  arrayObjetosValidate.forEach(element => newArrayStatusText.push(element.statusText));
+  for (let i = 0; i < newArrayStatusText.length; i++) {
+    if (newArrayStatusText[i] !== 'OK') {
+      broken ++;
+    } else {
+      broken = broken;
+    }
+  }
+  return {broken};
+};
+
--- src/controller/stats.spec.js
+++ src/controller/stats.spec.js
@@ -1,4 +1,4 @@
-import { statsLinks } from '../controller/stats.js';
+import { statsLinks, linksBroken } from '../controller/stats.js';
 
 const arrayObjLinks = [{
   href: 'https://www.google.com/',
@@ -19,8 +19,39 @@ const arrayObjLinks = [{
       'C:\\Users\\Micaela\\projectsLaboratoria\\LIM008-fe-md-links\\test\\prueba\\file.md'
 }];
 
+const arrayObjValidate = [{
+  href: 'https://www.google.com/',
+  text: 'https://www.google.com/',
+  file:
+        'C:\\Users\\Micaela\\projectsLaboratoria\\LIM008-fe-md-links\\test\\prueba\\file.md',
+  status: 200,
+  statusText: 'OK'
+},
+{
+  href: 'http://algo.com/2/3/',
+  text: 'http://algo.com/2/3/',
+  file:
+        'C:\\Users\\Micaela\\projectsLaboratoria\\LIM008-fe-md-links\\test\\prueba\\file.md',
+  status: 404,
+  statusText: 'Fail'
+},
+{
+  href: 'https://rpp.pe/',
+  text: 'https://rpp.pe/',
+  file:
+        'C:\\Users\\Micaela\\projectsLaboratoria\\LIM008-fe-md-links\\test\\prueba\\file.md',
+  status: 200,
+  statusText: 'OK'
+}];
+
 describe('statsLinks', () => {
   it('debería retornar el total de links y links unicos', () => {
     expect(statsLinks(arrayObjLinks)).toEqual({totalLinks: 3, uniqLinks: 3});
+  });
+});
+
+describe('linksBroken', () => {
+  it('debería retornar el total de links rotos', () => {
+    expect(linksBroken(arrayObjValidate)).toEqual({broken: 1});
   });
 });
